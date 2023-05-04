f9d0f86722f1c05d990bd68c5386b96f55922d86
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2023-05-04 13:19:24 +0000

    bug fix

--- active_learning_sampling.py
+++ active_learning_sampling.py
@@ -1,4 +1,4 @@
-# python active_learning_sampling.py --model-args-fp=/home/jupyter/sound_event_detection/logs/four_classes_highlamb/params.yaml --model-checkpoint-fp=/home/jupyter/sound_event_detection/logs/four_classes_highlamb/final_model.pt --name=debug_al --clips-per-file=3 --candidate-manifest-fp=/home/jupyter/al_crows_debug.csv
+# python active_learning_sampling.py --model-args-fp=/home/jupyter/sound_event_detection/logs/sweep3/params.yaml --model-checkpoint-fp=/home/jupyter/sound_event_detection/logs/sweep3/best_model.pt --name=active_learning_19_iter0 --clips-per-file=1 --candidate-manifest-fp=/home/jupyter/carrion_crows_data/call_detection_data.active_origsr/unannotated_info.csv --output-dir=/home/jupyter/sound_event_detection/active_learning_19_iter0
 
 import os
 import yaml
@@ -22,7 +22,10 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def sample_uncertainty(fp, model, al_args, args, rng):
   starts, durations, uncertainties = get_uncertainties(fp, model, al_args, args)
-    
+  
+  if len(uncertainties)<al_args.min_n_clips_in_file_required:
+    return [], [], [], []
+  
   uncertainty_cutoff = np.quantile(uncertainties, al_args.uncertainty_quantile)
   to_sample_idxs = np.nonzero(np.array(uncertainties) >= uncertainty_cutoff)[0]
   if len(to_sample_idxs)<al_args.clips_per_file:
@@ -31,6 +34,7 @@ def sample_uncertainty(fp, model, al_args, args, rng):
     sampled_idxs = rng.choice(to_sample_idxs, size = al_args.clips_per_file, replace = False)
         
   audio_clips_sampled = [librosa.load(fp, offset = starts[i], duration = durations[i], sr=args.sr, mono=True)[0] for i in sampled_idxs]
+  audio_clips_sampled = [x - np.mean(x) for x in audio_clips_sampled] # adjust for dc offset since clips will be merged together
   starts_sampled = [starts[i] for i in sampled_idxs]
   durations_sampled = [durations[i] for i in sampled_idxs]
   uncertainties_sampled = [uncertainties[i] for i in sampled_idxs]
@@ -93,10 +97,23 @@ def parse_al_args(al_args):
   parser.add_argument('--sample-duration', type = float, default = 30, help = 'duration of clip to sample for annotation, in seconds')
   parser.add_argument('--uncertainty-quantile', type = float, default = .95, help = 'controls how clips are sampled, closer to 1 means more preference for the most uncertain clips')
   parser.add_argument('--clips-per-file', type = int, default = 1, help = 'how many clips to sample per file')
+  parser.add_argument('--min-n-clips-in-file-required', type = int, default = 10, help = 'how many clips are required in a file before we sample from that file')
   parser.add_argument('--uncertainty-discount-factor', type = float, default = 0.8, help = 'geometric weighting of uncertainties. Uncertainties are sorted and then the lower ranking uncertainties count for less. Closer to 0 discourages sampling clips with lots of detections')
   parser.add_argument('--uncertainty-detection-threshold', type=float, default = 0.1, help = 'ignore detection peaks lower than this value, for the purpose of computing uncertainty')
   al_args = parser.parse_args()  
   return al_args
+
+def save_params(al_args):
+  """ Save a copy of the params used for this experiment """
+  params_file = os.path.join(al_args.output_dir, f"{al_args.name}_params.yaml")
+
+  args_dict = {}
+  for name in sorted(vars(al_args)):
+    val = getattr(al_args, name)
+    args_dict[name] = val
+
+  with open(params_file, "w") as f:
+    yaml.dump(args_dict, f)
   
   
 def main(al_args):
@@ -107,6 +124,11 @@ def main(al_args):
   
   if al_args.output_dir is None:
     setattr(al_args, 'output_dir', args.experiment_dir)
+    
+  if not os.path.exists(al_args.output_dir):
+    os.makedirs(al_args.output_dir)
+    
+  save_params(al_args)
   
   model = DetectionModel(args)
   
@@ -131,6 +153,7 @@ def main(al_args):
     fn = row['fn']
     print(f"Sampling clips from {fn}")
     a, s, d, u = sample_uncertainty(fp, model, al_args, args, rng)
+    
     output_audio.extend(a)
     start_times.extend(s)
     durations.extend(d)
