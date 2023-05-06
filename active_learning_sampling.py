1f21dad796cd3c55071974471aa400945e4678d1
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2023-05-06 17:42:46 +0000

    change active learning sampling strategy

--- active_learning_sampling.py
+++ active_learning_sampling.py
@@ -7,6 +7,7 @@ import sys
 import numpy as np
 import pandas as pd
 import torch
+import torchaudio
 from tqdm import tqdm
 import librosa
 import argparse
@@ -16,30 +17,37 @@ import soundfile as sf
 from util import load_params, set_seed
 from model import DetectionModel
 from evaluation import generate_predictions
-from data import get_single_clip_data
+from data import get_single_clip_data, crop_and_pad
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def sample_uncertainty(fp, model, al_args, args, rng):
+def sample_uncertainty(fp, model, al_args, args):
   starts, durations, uncertainties = get_uncertainties(fp, model, al_args, args)
   
-  if len(uncertainties)<al_args.min_n_clips_in_file_required:
-    return [], [], [], []
+  if len(uncertainties)==0:
+    return [], [], []
   
-  uncertainty_cutoff = np.quantile(uncertainties, al_args.uncertainty_quantile)
-  to_sample_idxs = np.nonzero(np.array(uncertainties) >= uncertainty_cutoff)[0]
-  if len(to_sample_idxs)<al_args.clips_per_file:
-    sampled_idxs = to_sample_idxs
-  else:
-    sampled_idxs = rng.choice(to_sample_idxs, size = al_args.clips_per_file, replace = False)
+  if len(uncertainties)<=al_args.clips_per_file:
+    return starts, durations, uncertainties
+  
+  unc_sorted = np.sort(uncertainties)[::-1]
+  uncertainty_cutoff = unc_sorted[al_args.clips_per_file-1]
+  
+  # sample above cutoff
+  sampled_idxs = np.nonzero(np.array(uncertainties) > uncertainty_cutoff)[0]
+  
+  # sample at cutoff, handle situation if there are ties in uncertainty
+  n_remaining_to_sample = al_args.clips_per_file - len(sampled_idxs)
+  borderline_sampled_idxs = np.nonzero(np.array(uncertainties) == uncertainty_cutoff)[0]
+  borderline_sampled_idxs = borderline_sampled_idxs[:n_remaining_to_sample]
+  
+  sampled_idxs = np.concatenate([sampled_idxs, borderline_sampled_idxs])
         
-  audio_clips_sampled = [librosa.load(fp, offset = starts[i], duration = durations[i], sr=args.sr, mono=True)[0] for i in sampled_idxs]
-  audio_clips_sampled = [x - np.mean(x) for x in audio_clips_sampled] # adjust for dc offset since clips will be merged together
   starts_sampled = [starts[i] for i in sampled_idxs]
   durations_sampled = [durations[i] for i in sampled_idxs]
   uncertainties_sampled = [uncertainties[i] for i in sampled_idxs]
   
-  return audio_clips_sampled, starts_sampled, durations_sampled, uncertainties_sampled
+  return starts_sampled, durations_sampled, uncertainties_sampled
 
 def get_uncertainties(audio_fp, model, al_args, args):
   dataloader = get_single_clip_data(audio_fp, args.clip_duration/2, args)
@@ -47,9 +55,6 @@ def get_uncertainties(audio_fp, model, al_args, args):
     return [], [], []
   
   predictions, _ = generate_predictions(model, dataloader, args)
-  
-  if 'Morado' in audio_fp:
-    np.save('/home/jupyter/test.npy', predictions)
   
   num_clips = int(np.floor((dataloader.dataset.duration - al_args.sample_duration) // al_args.sample_duration))
   
@@ -98,9 +103,8 @@ def parse_al_args(al_args):
   parser.add_argument('--output-dir', type=str, default=None)
   parser.add_argument('--candidate-manifest-fp', help='fp to tsv list of wav files to sample')
   parser.add_argument('--sample-duration', type = float, default = 30, help = 'duration of clip to sample for annotation, in seconds')
-  parser.add_argument('--uncertainty-quantile', type = float, default = .95, help = 'controls how clips are sampled, closer to 1 means more preference for the most uncertain clips')
+  parser.add_argument('--max-n-clips-to-sample', type = int, default = 120, help = 'how many clips to sample')
   parser.add_argument('--clips-per-file', type = int, default = 1, help = 'how many clips to sample per file')
-  parser.add_argument('--min-n-clips-in-file-required', type = int, default = 10, help = 'how many clips are required in a file before we sample from that file')
   parser.add_argument('--uncertainty-discount-factor', type = float, default = 0.8, help = 'geometric weighting of uncertainties. Uncertainties are sorted and then the lower ranking uncertainties count for less. Closer to 0 discourages sampling clips with lots of detections')
   parser.add_argument('--uncertainty-detection-threshold', type=float, default = 0.1, help = 'ignore detection peaks lower than this value, for the purpose of computing uncertainty')
   al_args = parser.parse_args()  
@@ -117,13 +121,32 @@ def save_params(al_args):
 
   with open(params_file, "w") as f:
     yaml.dump(args_dict, f)
+    
+def assemble_output_audio(output_log, al_args, args):
+  output_audio = []
+  for i, row in output_log.iterrows():
+    fp = row['audio_fp']
+    start = row['start_second']
+    duration = row['duration']
+    
+    audio, file_sr = librosa.load(fp, offset = start, duration = duration, sr = None, mono= True)
+    audio = audio-np.mean(audio)
+    audio = torch.from_numpy(audio)
+    
+    if file_sr != args.sr:
+      audio = torchaudio.functional.resample(audio, file_sr, args.sr) 
+      audio = crop_and_pad(audio, args.sr, duration)
+      
+    audio = audio.numpy()
+    output_audio.append(audio)
   
+  output_audio = np.concatenate(output_audio)
+  return output_audio
   
 def main(al_args):
   al_args = parse_al_args(al_args)
   args = load_params(al_args.model_args_fp)
   set_seed(al_args.seed)
-  rng = np.random.default_rng(al_args.seed)
   
   if al_args.output_dir is None:
     setattr(al_args, 'output_dir', args.experiment_dir)
@@ -148,8 +171,6 @@ def main(al_args):
   filenames = []
   filepaths = []
   uncertainties = []
-  output_audio = []
-
   
   for i, row in files_to_sample.iterrows():
     fp = row['audio_fp']
@@ -160,9 +181,8 @@ def main(al_args):
       continue
     print(f"Sampling clips from {fn}")
     
-    a, s, d, u = sample_uncertainty(fp, model, al_args, args, rng)
+    s, d, u = sample_uncertainty(fp, model, al_args, args)
     
-    output_audio.extend(a)
     start_times.extend(s)
     durations.extend(d)
     filenames.extend([fn for i in s])
@@ -175,9 +195,12 @@ def main(al_args):
                 'audio_fp' : filepaths,
                 'uncertainty' : uncertainties}
   output_log = pd.DataFrame(output_log)
-  output_log.to_csv(os.path.join(al_args.output_dir, f'{al_args.name}_active_learning_log.csv'))
   
-  output_audio = np.concatenate(output_audio)
+  # subselect
+  output_log = output_log.sort_values('uncertainty', ascending = False)[:al_args.max_n_clips_to_sample].reset_index()
+  
+  output_log.to_csv(os.path.join(al_args.output_dir, f'{al_args.name}_active_learning_log.csv'))
+  output_audio = assemble_output_audio(output_log, al_args, args)
   output_audio_fp = os.path.join(al_args.output_dir, f'{al_args.name}_active_learning_audio.wav')
   
   print(f"Saving outputs to {al_args.output_dir}")
