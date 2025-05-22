48c36d9fe5cdddaafe5cac0a526cd359db9a43bd
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2025-05-22 17:38:32 +0000

    clean up

--- dset_setup.sh
+++
@@ -1,5 +0,0 @@
-
-dset_name=$1
-
-echo python main.py project-setup --train-info-fp=datasets/${1}/formatted/train_info.csv --val-info-fp=datasets/${1}/formatted/val_info.csv --test-info-fp=datasets/${1}/formatted/test_info.csv --project-dir=projects/${1}_experiment
-python main.py project-setup --train-info-fp=datasets/${1}/formatted/train_info.csv --val-info-fp=datasets/${1}/formatted/val_info.csv --test-info-fp=datasets/${1}/formatted/test_info.csv --project-dir=projects/${1}_experiment
